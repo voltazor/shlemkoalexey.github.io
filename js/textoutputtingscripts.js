23250862954ded84e59a4272c791e29d35317cfd
ShlemkoAlexey <shlemko.alexey@gmail.com>
2015-12-18 12:34:34 +0200

    null

--- js/textoutputtingscripts.js
+++ js/textoutputtingscripts.js
@@ -1,9 +1,16 @@
-var outputText = "The Matrix is a 1999 American–Australian neo-noir science fiction action film written and directed by The Wachowskis, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano. It depicts a dystopian future in which reality as perceived by most humans is actually a simulated reality called \"the Matrix\", created by sentient machines to subdue the human population, while their bodies' heat and electrical activity are used as an energy source. Computer programmer \"Neo\" learns this truth and is drawn into a rebellion against the machines, which involves other people who have been freed from the \"dream world\".";
-
-
-
 $(document).ready(function(){
-  printTextWithDelay($('p'), outputText, 15);  
+  $(".button-to-output").click(function(){
+    
+    if ($("textarea").val().length !=0) {
+    $('.output-p').empty();
+    printTextWithDelay($('.output-p'), $("textarea").val(), 25);
+    $("textarea").val(function(){
+     $(this).empty();
+    });
+    }else{
+      alert("Textarea if empty, please write something!");
+    }
+  });
 });
 
 
