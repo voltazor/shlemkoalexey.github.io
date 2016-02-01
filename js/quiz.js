54119b8995141d9dbd4e54f9bb9aa568828fa552
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-02-01 17:48:16 +0200

    null

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,89 @@
 var questionCounter = 0;
 var correctCounter = 0;
 
-
+var solarSystemArray = [
+    {
+        name: "Меркурий",
+        distanceFromSun: 0.387, //a.e.
+        radius: 2439.7, //km
+        dayLength: 58.646, //days
+        yearLength: 87.97, //days
+        firstSpaceSpeed: 3.05, //km/s
+        surfaceTemperature: 700 //K
+    },
+    {
+        name: "Венера",
+        distanceFromSun: 0.723, //a.e.
+        radius: 6051.8, //km
+        dayLength: 243.02, //days
+        yearLength: 224.7, //days
+        firstSpaceSpeed: 7.328, //km/s
+        surfaceTemperature: 737 //K
+    },
+    {
+        name: "Земля",
+        distanceFromSun: 1, //a.e.
+        radius: 6371, //km
+        dayLength: 1, //days
+        yearLength: 365, //days
+        firstSpaceSpeed: 7.9, //km/s
+        surfaceTemperature: 287 //K    
+    },
+    {
+        name: "Марс",
+        distanceFromSun: 1.52, //a.e.
+        radius: 3396.9, //km
+        dayLength: 1.0025, //days
+        yearLength: 687, //days
+        firstSpaceSpeed: 3.6, //km/s
+        surfaceTemperature: 210 //K
+    },
+    {
+        name: "Юпитер",
+        distanceFromSun: 5.2, //a.e.
+        radius: 71400, //km
+        dayLength: 0.41, //days
+        yearLength: 4328.9, //days
+        firstSpaceSpeed: 42.073, //km/s
+        surfaceTemperature: 128 //K
+    },
+    {
+        name: "Сатурн",
+        distanceFromSun: 8, //a.e.
+        radius: 60300, //km
+        dayLength: 0.4372, //days
+        yearLength: 10759, //days
+        firstSpaceSpeed: 25, //km/s
+        surfaceTemperature: 134 //K
+    },
+    {
+        name: "Уран",
+        distanceFromSun: 19.19, //a.e.
+        radius: 25559, //km
+        dayLength: 0.718, //days
+        yearLength: 30660, //days
+        firstSpaceSpeed: 15.6, //km/s
+        surfaceTemperature: 49 //K
+    },
+    {
+        name: "Нептун",
+        distanceFromSun: 30.01, //a.e.
+        radius: 24764, //km
+        dayLength: 0.671, //days
+        yearLength: 60148, //days
+        firstSpaceSpeed: 16.7, //km/s
+        surfaceTemperature: 55 //K
+    },
+    {
+        name: "Плутон",
+        distanceFromSun: 39.482, //a.e.
+        radius: 1187, //km
+        dayLength: 6.387, //days
+        yearLength: 90490, //days
+        firstSpaceSpeed: 0.855, //km/s
+        surfaceTemperature: 50 //K
+    }
+];
 
 
 
@@ -27,4 +109,5 @@ function correctCounterUpdate(){
 
 $(document).ready(function(){
     $("#theme-selector").prop('value', false);
+    alert(solarSystemArray[5].firstSpaceSpeed);
 });
