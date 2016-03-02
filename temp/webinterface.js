4f121c827b860e14f86f0bbe423bea1153a81849
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-03-02 16:02:37 +0200

    small fixes and web-interface page started

---
+++ temp/webinterface.js
@@ -0,0 +1,21 @@
+var linksArray = {
+    link1 : "http://blog.eney.solutions/wp-content/uploads/sites/3/2011/11/placeholder1.jpg",
+    link2 : "234",
+    link3 : "345",
+    link4 : "456",
+    link5 : "567",
+    link6 : "678",
+    link7 : "789",
+    link8 : "890",
+    link9 : "90-",
+    link10 : "0-=",
+    link11 : "-=",
+    link12 : "="
+}
+
+
+
+
+$(document).ready(function(){
+  
+});
