f2c822cf167236fc8e115c8e91a243a72ea4570b
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-03-04 12:47:57 +0200

    transition fix

--- temp/webinterface.js
+++ temp/webinterface.js
@@ -1,12 +1,12 @@
 var linksArray = {
-    link1: "https://placeholdit.imgix.net/~text?txtsize=28&bg=0099ff&txtclr=ffffff&txt=300%C3%97300&w=300&h=300&fm=png",
-    link2: "http://sqlbook.ru/wp-content/uploads/2014/08/wpid-pravil-nye-pleysholdery_i_2.jpg",
-    link3: "http://shustov.su/images/items/html5placeholder.jpg",
-    link4: "http://cs7.pikabu.ru/images/big_size_comm/2014-06_4/1403167186818.jpeg",
-    link5: "http://blog.eney.solutions/wp-content/uploads/sites/3/2011/11/placeholder1.jpg",
-    link6: "http://www.placeholder.org/images/Placeholder-NameIs-LogoLinks.gif",
-    link7: "http://spucweb.com/wp-content/uploads/picture-placeholder-female.gif",
-    link8: "http://shandermanshop.com/wp-content/themes/WCM010013/images/megnor/placeholder.png",
+    link1: "http://lorempixel.com/400/300/",
+    link2: "http://lorempixel.com/300/400/",
+    link3: "http://lorempixel.com/g/400/300/",
+    link4: "http://lorempixel.com/g/300/400/",
+    link5: "http://lorempixel.com/500/250/",
+    link6: "http://lorempixel.com/g/500/250/",
+    link7: "http://lorempixel.com/250/500/",
+    link8: "http://lorempixel.com/g/250/500/",
     link9: "http://lorempixel.com/g/400/200/",
     link10: "http://lorempixel.com/200/400/",
     link11: "http://lorempixel.com/g/200/400/",
@@ -15,20 +15,11 @@ var linksArray = {
 
 
 $(document).ready(function () {
-    //здесь мы определяем размеры изображений
+
     for (var i = 1; i <= 12; i++) {
         widerOrHigher(linksArray["link"+i], $("#image" + i));
-    };
-
-
-    //сделать функцию, которой передается ссылка и блок,
-    // в котором фон - это изображение, чтоб она ставила этому блоку соответствующий класс.
-
-    for (var i = 1; i <= 12; i++) {
         $(("#image" + i)).css("background", "url(" + linksArray[("link" + i)] + ") no-repeat");
-
-        //$(("#image" + i)).addClass("wider");
-    }
+    };
 });
 
 
