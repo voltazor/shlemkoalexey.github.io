e728113bd2a2694ce0ba6592f203e728fd1b075d
ShlemkoAlexey <ShlemkoAlexey@gmail.com>
2016-03-14 19:03:21 +0200

    test opacity in combat

--- temp/webinterface.js
+++ temp/webinterface.js
@@ -1,4 +1,18 @@
 var linksArray = {
+    link1: "http://36.media.tumblr.com/74c4e7a08ef4d797e8cd286bbf7492ae/tumblr_o3sixgNzIQ1shlzkjo1_1280.jpg",
+    link2: "http://40.media.tumblr.com/6c431e10e2c5177b9121badb38b548db/tumblr_o3siqzecKm1s87941o1_500.jpg",
+    link3: "http://41.media.tumblr.com/0154ff588d18c11f6bf116caf2f6caea/tumblr_o3sis5EuSI1s87941o1_500.jpg",
+    link4: "http://41.media.tumblr.com/1c5db1752bbefd38ab4b468f6adda6a5/tumblr_o3siy3lZoJ1shlzkjo1_1280.jpg",
+    link5: "http://36.media.tumblr.com/44076f791f7ff5df16ee3ec2b6fa403d/tumblr_o3sipijxtG1s87941o1_1280.jpg",
+    link6: "http://41.media.tumblr.com/a540013d13bc862963a119900ad16916/tumblr_o3sipsHX6j1s87941o1_1280.jpg",
+    link7: "http://41.media.tumblr.com/7b6464a3956db160401c94ea9665957c/tumblr_o3kkvaf3NP1s87941o1_400.jpg",
+    link8: "http://40.media.tumblr.com/11ea77ca7527ae69bf0ade25d7d2b6ff/tumblr_o3mn02YqhU1s37ot7o1_1280.jpg",
+    link9: "http://49.media.tumblr.com/f89825cd4887bd18a7cd8d9dc8242b32/tumblr_o3eh8eTkIS1s0si5no1_400.gif",
+    link10: "http://45.media.tumblr.com/4208f488c9c1dbbb958142b66ae52137/tumblr_npybzzhdu51sysgwno1_250.gif",
+    link11: "http://49.media.tumblr.com/e283b9d6793f62e685cf215a00ed0f50/tumblr_nupo73Pz1u1t3ctrho1_250.gif",
+    link12: "http://45.media.tumblr.com/adc1dae467b02702acfa08005da33f4b/tumblr_n2j1dfCWep1sqkjcto3_400.gif"
+};/*
+var linksArray = {
     link1: "http://lorempixel.com/400/300/",
     link2: "http://lorempixel.com/300/400/",
     link3: "http://lorempixel.com/g/400/300/",
@@ -11,34 +25,33 @@ var linksArray = {
     link10: "http://lorempixel.com/200/400/",
     link11: "http://lorempixel.com/g/200/400/",
     link12: "http://lorempixel.com/400/200/"
-}
-
-
+};
+*/
 $(document).ready(function () {
-    
+
     $(".gallery-section").append("<div class=\"section-shadow\"></div>");
     for (var i = 1; i <= 12; i++) {
-        widerOrHigher(linksArray["link"+i], $("#image" + i));
+        widerOrHigher(linksArray["link" + i], $("#image" + i));
         $(("#image" + i)).css("background", "url(" + linksArray[("link" + i)] + ") no-repeat");
-        
-        $("#carousel-main").append("<div><img src=\""+linksArray[("link" + i)]+"\"></div>");
-    };
+
+        $("#carousel-main").append("<div><img src=\"" + linksArray[("link" + i)] + "\"></div>");
+    }
+
     createCarousel($('#carousel-main'));
-    $(".gallery-section").click(function(){
+    $(".gallery-section").click(function () {
         $('#carousel-main').fadeIn();
         var id = $(this).attr("id");
         id = Number(id.substring(5));
         console.log(id);
-        $('#carousel-main').slick("slickGoTo", id-1, true);
+        $('#carousel-main').slick("slickGoTo", id - 1, true);
         mainBlockOpacityOn();
     });
-    
+
 
 });
 
 
-
-function widerOrHigher(link, image){
+function widerOrHigher(link, image) {
     image.append("<div class=\"section-preloader\"><img src=\"preloader.gif\"></div>");
     var src = link;
     var img = document.createElement("img");
@@ -46,53 +59,54 @@ function widerOrHigher(link, image){
     img.onload = function () {
         console.log("params getted");
         if (img.width >= img.height) {
-            image.addClass("wider"); 
-            console.log("wider"); 
-        }else{
-            image.addClass("higher"); 
-            console.log("higher");      
-        };
-    image.find(".section-preloader").remove();
+            image.addClass("wider");
+            console.log("wider");
+        } else {
+            image.addClass("higher");
+            console.log("higher");
+        }
+
+        image.find(".section-preloader").remove();
     };
 }
 
-function createCarousel(carouselBlock){
+function createCarousel(carouselBlock) {
     carouselBlock.slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      respondTo: "slider",
-      arrows: false      
+        dots: true,
+        infinite: true,
+        speed: 300,
+        respondTo: "slider",
+        arrows: false
     });
     carouselBlock.fadeOut();
     carouselBlock.append("<button id=\"carousel-button-prev\">←</button>");
     carouselBlock.append("<button id=\"carousel-button-next\">→</button>");
     carouselBlock.append("<button id=\"carousel-button-close\">×</button>");
-    $("#carousel-button-prev").on("click", function(){
+    $("#carousel-button-prev").on("click", function () {
         carouselBlock.slick("slickPrev");
     });
-    $("#carousel-button-next").on("click", function(){
+    $("#carousel-button-next").on("click", function () {
         carouselBlock.slick("slickNext");
     });
     /*по сути следующий ивент теперь не нужен*/
-    $("#carousel-button-close").on("click", function(){
+    $("#carousel-button-close").on("click", function () {
         carouselBlock.fadeOut();
         mainBlockOpacityOff();
     });
 
-    $("#carousel-main").bind('click', function(e) {
-        if(!$(e.target).is('#carousel-button-prev, #carousel-button-next, img, button')) {
-        carouselBlock.fadeOut();
-        mainBlockOpacityOff();         
-    }
-});
+    $("#carousel-main").bind('click', function (e) {
+        if (!$(e.target).is('#carousel-button-prev, #carousel-button-next, img, button')) {
+            carouselBlock.fadeOut();
+            mainBlockOpacityOff();
+        }
+    });
 
 
 }
 
-function mainBlockOpacityOn(){
-    $("#main-section").fadeTo(500, 0.3);
+function mainBlockOpacityOn() {
+    $("#main-section").fadeTo(500, 0.15);
 }
-function mainBlockOpacityOff(){
-    $("#main-section").fadeTo(500, 1);  
+function mainBlockOpacityOff() {
+    $("#main-section").fadeTo(500, 1);
 }
