44a7c6993e2ad30ce052ccf847b7f6a99c709480
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-03-25 12:26:49 +0200

    todo.cookie

--- js/todo.js
+++ js/todo.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
-   
-    $("#add-button").click(function(){
-        if ($("#add-input").val().length>0) {
-            $(".to-do-list").append("<li>" + $("#add-input").val() +"</li>");
-            $("#add-input").val(function(){
-                $(this).empty();
-            });
-        };
-    });
+    $.cookie('qwer', 'rewq');
+    console.log($.cookie("qwer"));
+});
 
-    $(document).on("click", ".to-do-list li", function(){
-        $(this).appendTo(".done-list");
-    });
+$("#add-button").click(function(){
+    if ($("#add-input").val().length>0) {
+        $(".to-do-list").append("<li>" + $("#add-input").val() +"</li>");
+        $("#add-input").val(function(){
+            $(this).empty();
+        });
+    };
+});
+$(document).on("click", ".to-do-list li", function(){
+    $(this).appendTo(".done-list");
+});
 
-    $(document).on("click", ".done-list li", function(){
-        $(this).appendTo(".to-do-list");
-    });
+$(document).on("click", ".done-list li", function(){
+    $(this).appendTo(".to-do-list");
+});
 
-    $("#remove-tasks-button").click(function(){
-        $(".done-list li").remove();
-    });
+$("#remove-tasks-button").click(function(){
+    $(".done-list li").remove();
 });
