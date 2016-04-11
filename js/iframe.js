51349997d1d599e90848c40846eec4f41a625be5
ShlemkoAlexey <shlemko.alexey@gmail.com>
2016-04-11 17:56:29 +0300

    lil fxs

--- js/iframe.js
+++ js/iframe.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   outputReciever();
-  var nowDate = new Date();  
+  var nowDate = new Date();
   var requestAdress = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Ftrofey.net%2Findex.php%3Foption%3Dcom_tvguide%26view%3Dprograms%26format%3Draw%26video%3D1%26date%3D"+stringFromDate(nowDate)+"'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
   $.getJSON(requestAdress)
-  .done(function(data){     
+  .done(function(data){
     for (var i = 0; i < data.query.results.body.table.tbody.tr.length; i++) {
      var programString = "<div class=\"program\">"+unicodeToChar(data.query.results.body.table.tbody.tr[i].th)+unicodeToChar(data.query.results.body.table.tbody.tr[i].td) + "</div>";
      $(".tele-frame").append(clearStringFromTrash(programString));
@@ -19,12 +19,13 @@ $(document).ready(function(){
     $(".sms-frame").append(nessesaryString + "///");
   }
   console.log($(".sms-frame").html().length);
-  $(".sms-frame").html($(".sms-frame").html().slice(0, $(".sms-frame").html().length-3));  
-});  
+  $(".sms-frame").html($(".sms-frame").html().slice(0, $(".sms-frame").html().length-3));
+  $("#send-sms-btn").removeClass("disabled");  
+});
 });
 $("#send-sms-btn").click(function(){
   var smsMessage = $(".sms-frame").html();
-  var login = "farewell47";  //логин  
+  var login = "farewell47";  //логин
   var password = "123456"; //пароль
   var phoneNumber = $("#phone-select").val(); //номер телефона
   console.log(smsMessage);
@@ -40,7 +41,7 @@ $("#send-sms-btn").click(function(){
   });
   $(this).html("Отправлено");
   $(this).addClass("disabled");
-  $(this).unbind();  
+  $(this).unbind();
 });
 
 
@@ -49,7 +50,7 @@ $("#phone-select").click(function(){
 });
 
 function unicodeToChar(text) {
- return text.replace(/\\u[\dA-F]{4}/gi, 
+ return text.replace(/\\u[\dA-F]{4}/gi,
   function (match) {
    return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
  });
@@ -73,7 +74,7 @@ function clearStringFromTrash(string){
 
 function makeStringShorter(string){
   string = string.replace("Дальнийкордон", "Кордон");
-  string = string.replace("Нипуха,нипера!", "Нипуха");  
+  string = string.replace("Нипуха,нипера!", "Нипуха");
   return string;
 }
 
@@ -84,10 +85,10 @@ function stringFromDate(date){
 
 function outputReciever(){
   if ($('#phone-select').val()==="380938717845") {
-    $("#reciever").html("Дедушка");    
+    $("#reciever").html("Дедушка");
   }else if ($('#phone-select').val()==="380934531121") {
-    $("#reciever").html("Лёша");    
+    $("#reciever").html("Лёша");
   }else if ($('#phone-select').val()==="380979627111") {
-    $("#reciever").html("Юра");    
+    $("#reciever").html("Юра");
   };
 }
